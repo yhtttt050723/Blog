1a97dc9b0732fb6e51495d0f4e7ccbc6e13b1fd4
yhtttt050723 <519275808@qq.com>
2025-03-02 12:51:24 +0800

    后端和网页部分

---
+++ Users/xxx/workspace/schooltalk/pages/chat/chat.vue
@@ -0,0 +1,21 @@
+<template>
+  <view class="chat-container">
+    <text>好友交流页面</text>
+  </view>
+</template>
+
+<script>
+export default {
+  name: 'Chat',
+};
+</script>
+
+<style scoped>
+.chat-container {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  height: 100vh;
+}
+</style>
